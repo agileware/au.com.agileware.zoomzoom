f3fcfe8bdb49ec35835bc42afc4ea64843a21b18
agileware-fj <agileware-fj@users.noreply.github.com>
2025-03-26 18:44:22 +0000

    CIVIBLD-287 [AUTO] civix upgraded
    Files changed:
    M	CRM/Zoomzoom/Upgrader.php
    D	CRM/Zoomzoom/Upgrader/Base.php
    M	info.xml
    A	mixin/lib/civimix-schema@5.80.2/pathload.main.php
    A	mixin/lib/civimix-schema@5.80.2/src/AutomaticUpgrader.php
    A	mixin/lib/civimix-schema@5.80.2/src/CiviMixSchema.php
    A	mixin/lib/civimix-schema@5.80.2/src/DAO.php
    A	mixin/lib/civimix-schema@5.80.2/src/SchemaHelper.php
    A	mixin/lib/civimix-schema@5.80.2/src/SchemaHelperInterface.php
    A	mixin/lib/civimix-schema@5.80.2/src/SqlGenerator.php
    D	sql/auto_uninstall.sql
    M	zoomzoom.civix.php
    M	zoomzoom.php

--- zoomzoom.php
+++ zoomzoom.php
@@ -18,15 +18,6 @@ function zoomzoom_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function zoomzoom_civicrm_xmlMenu(&$files) {
-  _zoomzoom_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -36,48 +27,12 @@ function zoomzoom_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function zoomzoom_civicrm_postInstall() {
-  _zoomzoom_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function zoomzoom_civicrm_uninstall() {
-  _zoomzoom_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function zoomzoom_civicrm_enable() {
   _zoomzoom_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function zoomzoom_civicrm_disable() {
-  _zoomzoom_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function zoomzoom_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _zoomzoom_civix_civicrm_upgrade($op, $queue);
 }
 
 /**
@@ -123,61 +78,6 @@ function zoomzoom_civicrm_managed(&$entities) {
     ],
   ];
 
-  _zoomzoom_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function zoomzoom_civicrm_caseTypes(&$caseTypes) {
-  _zoomzoom_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function zoomzoom_civicrm_angularModules(&$angularModules) {
-  _zoomzoom_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function zoomzoom_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _zoomzoom_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function zoomzoom_civicrm_entityTypes(&$entityTypes) {
-  _zoomzoom_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_themes().
- */
-function zoomzoom_civicrm_themes(&$themes) {
-  _zoomzoom_civix_civicrm_themes($themes);
 }
 
 /**
